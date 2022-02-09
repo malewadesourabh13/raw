a04605ada681c91f573d05a271f0a2e592f5c3ed
Sourabh <100rabhMalewade@gmail.com>
2022-02-09 16:36:36 +0530

    routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,12 @@ const { protect, admin } = require('../middleware/authMiddleware');
 
 
 router.post('/', registerUser )
-router.route('/').get(protect, admin, getUsers)
+router.route('/admin').get(protect, admin, getUsers)
 router.post('/login',authUser )
 router.route('/profile').get(protect, getUserProfile)
 router.route('/profile').put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
-router.route('/:id').get(protect, admin, getUserById)
-router.route('/:id').put(protect, admin, updateUser)
+router.route('/admin/:id').delete(protect, admin, deleteUser)
+router.route('/admin/:id').get(protect, admin, getUserById)
+router.route('/admin/:id').put(protect, admin, updateUser)
 
 module.exports = router;
